fd8646cc9aca3e1c362c079ed28edc7fc9026a77
Arvind <arvindbhandari123456@gmail.com>
2024-02-27 22:21:22 +0530

    changed bgcolor

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['scontent.fdel1-5.fna.fbcdn.net', 'platform-lookaside.fbsbx.com'],
+    domains: ['scontent.fdel1-5.fna.fbcdn.net', 'platform-lookaside.fbsbx.com', 'scontent.fdel27-4.fna.fbcdn.net'],
   },
 };
 
